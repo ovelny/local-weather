04f6d0431df4f89c15e126c8321dfbd3ba3b9d53
Ovelny <this.is.ovelny@gmail.com>
2016-08-29 22:21:20 +0200

    API get

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-$(document).ready(function(){
-	
-	$.ajax({
-		url: "",
-		type: "GET",
-		dataType: "",
-		success: function(getLocation) {
-			console.log(getWeather);
-		} 
+"use strict";
+
+$(document).ready(function() {
+
+	$.get('http://ip-api.com/json', function(jsonGeoLoc) {
+		var city = jsonGeoLoc.city;
+		var countryCode = jsonGeoLoc.countryCode;
+
+		$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + countryCode + '&appid=' + apiKey, function(jsonWeather) {
+			console.log(jsonWeather);
+			var weather = jsonWeather.weather[0].description;
+			var tempKelvin = jsonWeather.main.temp;
+			var tempCelsius = Math.round(tempKelvin - 273.15);
+			var tempFahren = Math.round(tempKelvin * 9 / 5 - 459.67);
+		});
+
 	});
+
+
+
 });
