0ea3bc8c15ab2a75cfdf63ebf2097c0aa4b3513e
Ovelny <this.is.ovelny@gmail.com>
2016-09-06 07:43:48 +0200

    temp & weather status display

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
 
 		$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + countryCode + '&appid=' + apiKey, function(jsonWeather) {
 			console.log(jsonWeather);
+			
 			var weather = jsonWeather.weather[0].description;
 			var tempKelvin = jsonWeather.main.temp;
 			var tempCelsius = Math.round(tempKelvin - 273.15);
 			var tempFahren = Math.round(tempKelvin * 9 / 5 - 459.67);
+
+			$(".weather-status").html(tempCelsius + "°C / " + weather);
 		});
 
 	});
