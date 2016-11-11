bcf3b2d100a07ecc8ce2fda7019b494f5118ec62
Ovelny <this.is.ovelny@gmail.com>
2016-11-12 00:04:52 +0100

    no semicolons

--- script.js
+++ script.js
@@ -4,40 +4,40 @@ $(document).ready(function() {
 
 	// Get city and country code from ip-api
 	$.get('http://ip-api.com/json', function(jsonGeoLoc) {
-		let city = jsonGeoLoc.city;
-		let countryCode = jsonGeoLoc.countryCode;
+		let city = jsonGeoLoc.city
+		let countryCode = jsonGeoLoc.countryCode
 
 		// Get current weather with openweather API
 		$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + countryCode + '&appid=' + apiKey, function(jsonWeather) {
-			console.log(jsonWeather);
+			console.log(jsonWeather)
 			
-			let weather = jsonWeather.weather[0].description;
-			let tempKelvin = jsonWeather.main.temp;
-			let tempCelsius = Math.round(tempKelvin - 273.15);
-			let tempFahren = Math.round(tempKelvin * 9 / 5 - 459.67);
+			let weather = jsonWeather.weather[0].description
+			let tempKelvin = jsonWeather.main.temp
+			let tempCelsius = Math.round(tempKelvin - 273.15)
+			let tempFahren = Math.round(tempKelvin * 9 / 5 - 459.67)
 
 			// Display weather info in Celsius
-			$(".weather-status").html(tempCelsius + "°C <br>" + weather);
+			$(".weather-status").html(tempCelsius + "°C <br>" + weather)
 
 			// Toggle between Fahrenheit and Celsius on click
 			$("button").click(function() {
 				
-				$("button").toggleClass("Fahrenheit");
+				$("button").toggleClass("Fahrenheit")
 
 				if ($("button").hasClass("Fahrenheit")) {
-                    $(".weather-status").html(tempFahren + "°F <br>" + weather);
+                    $(".weather-status").html(tempFahren + "°F <br>" + weather)
 				}
 
 				else {
-					$(".weather-status").html(tempCelsius + "°C <br>" + weather);
+					$(".weather-status").html(tempCelsius + "°C <br>" + weather)
 				}
-            });
+            })
 
 
-		});
+		})
 
-	});
+	})
 
 
 
-});
+})
