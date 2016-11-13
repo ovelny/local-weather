58a59321b6b0c3ec58783a93c94fd7acee71aadc
Ovelny <this.is.ovelny@gmail.com>
2016-11-13 03:14:31 +0100

    styling changes

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
 			console.log(jsonWeather.weather[0].icon)
 			
 			let weather = jsonWeather.weather[0].description
-			let icon = parseInt(jsonWeather.weather[0].icon)
-			console.log(icon)
+			let icon = jsonWeather.weather[0].icon
 			let tempKelvin = jsonWeather.main.temp
 			let tempCelsius = Math.round(tempKelvin - 273.15)
 			let tempFahren = Math.round(tempKelvin * 9 / 5 - 459.67)
 
-			// Display weather info in Celsius
-			$(".weather-status").html(tempCelsius + "°C <br>" + weather)
+			// Display weather info in Celsius & description
+			$(".weather-temp").html(tempCelsius + "°C")
+			$(".weather-description").html(weather)
 
 			// Toggle between Fahrenheit and Celsius on click
 			$("#temp-switch").click(function() {
@@ -28,38 +28,100 @@ $(document).ready(function() {
 				$(".temp-switch").toggleClass("Fahrenheit")
 
 				if ($(".temp-switch").hasClass("Fahrenheit")) {
-					$(".weather-status").html(tempFahren + "°F <br>" + weather)
+					$(".weather-temp").html(tempFahren + "°F")
 				}
 
 				else {
-					$(".weather-status").html(tempCelsius + "°C <br>" + weather)
+					$(".weather-temp").html(tempCelsius + "°C")
 				}
             })
 
 			// Choose icon based on openweather's icon number
 			switch (icon) {
-				case 1:
+
+				// Day icons
+				case "01d":
 					$(".wi").addClass("wi-day-sunny")
 					break
-				case 2:
-				case 3:
-				case 4:
+				case "02d":
+				case "03d":
+				case "04d":
 					$(".wi").addClass("wi-day-cloudy")
 					break
-				case 9:
+				case "09d":
 					$("wi").addClass("wi-day-showers")
 					break
-				case 10:
+				case "10d":
 					$(".wi").addClass("wi-day-rain")
 					break
-				case 11:
+				case "11d":
 					$(".wi").addClass("wi-day-lightning")
 					break
-				case 13:
+				case "13d":
 					$(".wi").addClass("wi-day-snow")
 					break
-				case 50:
+				case "50d":
 					$(".wi").addClass("wi-day-fog")
+					break
+
+				// Night icons
+				case "01n":
+					$(".wi").addClass("wi-night-sunny")
+					break
+				case "02n":
+				case "03n":
+				case "04n":
+					$(".wi").addClass("wi-night-cloudy")
+					break
+				case "09n":
+					$("wi").addClass("wi-night-showers")
+					break
+				case "10n":
+					$(".wi").addClass("wi-night-rain")
+					break
+				case "11n":
+					$(".wi").addClass("wi-night-lightning")
+					break
+				case "13n":
+					$(".wi").addClass("wi-night-snow")
+					break
+				case "50n":
+					$(".wi").addClass("wi-night-fog")
+					break
+				}
+
+			// Comments related to current weather
+			switch (icon) {
+
+				case "01d":
+				case "01n":
+					$(".weather-comment").html("Bright day and shiny ideas!")
+					break
+				case "02d":
+				case "02n":
+				case "03d":
+				case "03n":
+				case "04d":
+				case "04n":
+					$(".weather-comment").html("Have a nice day!")
+					break
+				case "09d":
+				case "09n":
+				case "10d":
+				case "10n":
+					$(".weather-comment").html("You should take an umbrella!")
+					break
+				case "11d":
+				case "11n":
+					$(".weather-comment").html("Get inside, grab a blanket and a hot coffee.<br>It's going to get better.")
+					break
+				case "13d":
+				case "13n":
+					$(".weather-comment").html("Snooooow. Snow!")
+					break
+				case "50d":
+				case "50n":
+					$(".weather-comment").html("Be careful if you drive!")
 					break
 			}
 
